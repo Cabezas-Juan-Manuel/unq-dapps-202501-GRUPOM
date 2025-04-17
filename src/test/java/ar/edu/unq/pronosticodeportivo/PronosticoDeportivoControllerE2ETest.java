df4de495be626f4c756b2f1b5103bf5c148e5501
Matias Laime <matulai1221@gmail.com>
2025-04-17 20:04:53 -0300

    Fix: test errors with the new security added

--- src/test/java/ar/edu/unq/pronosticodeportivo/PronosticoDeportivoControllerE2ETest.java
+++ src/test/java/ar/edu/unq/pronosticodeportivo/PronosticoDeportivoControllerE2ETest.java
@@ -1,13 +1,15 @@
 package ar.edu.unq.pronosticodeportivo;
 
 import ar.edu.unq.pronosticodeportivo.model.Player;
+import ar.edu.unq.pronosticodeportivo.service.UserService;
+import ar.edu.unq.pronosticodeportivo.webservice.Dtos.RegisterDto;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -20,15 +22,40 @@ public class PronosticoDeportivoControllerE2ETest {
     @Autowired
     private TestRestTemplate restTemplate;
 
+    @Autowired
+    private UserService userService;
+
+    @AfterEach
+    void cleanUsers() {
+        userService.deleteUsers();
+    }
+
     @Test
-    void getPlayersTest() {
+    void getPlayersFromTeamTest() {
         String baseUrl = "http://localhost:" + port;
         String teamName = "bayern munich";
 
-        TestRestTemplate authenticatedRestTemplate = restTemplate.withBasicAuth("user", "1234");
+        RegisterDto newUser = new RegisterDto();
+        newUser.setName("user");
+        newUser.setPassword("password$1");
 
-        ResponseEntity<Player[]> response = authenticatedRestTemplate.getForEntity(
+        ResponseEntity<Void> registerResponse = restTemplate.postForEntity(
+                baseUrl + "/auth/register",
+                newUser,
+                Void.class
+        );
+
+        String token = registerResponse.getHeaders().getFirst("Authorization");
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token.replace("Bearer ", ""));
+
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Player[]> response = restTemplate.exchange(
                 baseUrl + "/pronosticoDeportivo/team/" + teamName + "/players",
+                HttpMethod.GET,
+                entity,
                 Player[].class
         );
 

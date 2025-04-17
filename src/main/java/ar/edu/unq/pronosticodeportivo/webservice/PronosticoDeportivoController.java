942c106d862b65bed7daf22bdd6f85faa34cbea0
Matias Laime <matulai1221@gmail.com>
2025-04-17 20:03:24 -0300

    Add: handlers for diffents status codes

--- src/main/java/ar/edu/unq/pronosticodeportivo/webservice/PronosticoDeportivoController.java
+++ src/main/java/ar/edu/unq/pronosticodeportivo/webservice/PronosticoDeportivoController.java
@@ -1,13 +1,16 @@
 package ar.edu.unq.pronosticodeportivo.webservice;
 
-import ar.edu.unq.pronosticodeportivo.model.Player;
 import ar.edu.unq.pronosticodeportivo.service.integration.WhoScoredService;
-import ar.edu.unq.pronosticodeportivo.utils.JsonParser;
+import ar.edu.unq.pronosticodeportivo.utils.ApiError;
+import ar.edu.unq.pronosticodeportivo.model.Player;
+
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.http.HttpStatus;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
@@ -16,9 +19,22 @@ import java.util.List;
 public class PronosticoDeportivoController {
 
     @GetMapping("/team/{teamName}/players")
-    public ResponseEntity<List<Player>> getTeamPlayers(@PathVariable String teamName) {
-        String jsonString = WhoScoredService.getDataFromTableOnWeb(teamName, "team", "team-players");
-        List<Player> players = JsonParser.fromJsonToPlayerList(jsonString);
-        return ResponseEntity.ok().body(players);
+    public ResponseEntity<?> getPlayersFromTeam(@PathVariable String teamName) {
+        try {
+            List<Player> players = WhoScoredService.getPlayersFromTeam(teamName);
+
+            if (players.isEmpty()) {
+                ApiError error = new ApiError(HttpStatus.NOT_FOUND.value(), "No players found for team: " + teamName);
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+            }
+            return ResponseEntity.ok(players);
+
+        } catch (IllegalArgumentException e) {
+            ApiError error = new ApiError(HttpStatus.BAD_REQUEST.value(), e.getMessage());
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+        } catch (Exception e) {
+            ApiError error = new ApiError(HttpStatus.INTERNAL_SERVER_ERROR.value(), "An error occurred while retrieving players");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(error);
+        }
     }
 }

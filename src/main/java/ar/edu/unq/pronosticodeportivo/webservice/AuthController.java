25dd3af2287f08268266011ded488b3c9a8153a8
Matias Laime <matulai1221@gmail.com>
2025-04-17 20:35:08 -0300

    Refactor: Refactor: change to contructor inyection and modify authvalidator call

--- src/main/java/ar/edu/unq/pronosticodeportivo/webservice/AuthController.java
+++ src/main/java/ar/edu/unq/pronosticodeportivo/webservice/AuthController.java
@@ -1,45 +1,53 @@
 package ar.edu.unq.pronosticodeportivo.webservice;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import ar.edu.unq.pronosticodeportivo.model.User;
-import ar.edu.unq.pronosticodeportivo.security.JwtService;
-import ar.edu.unq.pronosticodeportivo.utils.AuthValidator;
-import ar.edu.unq.pronosticodeportivo.utils.UserMapper;
-import ar.edu.unq.pronosticodeportivo.webservice.Dtos.LoginDto;
 import ar.edu.unq.pronosticodeportivo.webservice.Dtos.RegisterDto;
+import ar.edu.unq.pronosticodeportivo.webservice.Dtos.LoginDto;
 import ar.edu.unq.pronosticodeportivo.webservice.Dtos.UserDto;
 import ar.edu.unq.pronosticodeportivo.service.UserService;
+import ar.edu.unq.pronosticodeportivo.utils.AuthValidator;
+import ar.edu.unq.pronosticodeportivo.security.JwtService;
+import ar.edu.unq.pronosticodeportivo.utils.UserMapper;
+import ar.edu.unq.pronosticodeportivo.model.User;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
-@RequiredArgsConstructor
 public class AuthController {
+
+    private final JwtService jwtService;
+    private final UserService userService;
+
     @Autowired
-    private JwtService jwtService;
-    @Autowired
-    private UserService userService;
+    public AuthController(JwtService jwtService, UserService userService) {
+        this.jwtService = jwtService;
+        this.userService = userService;
+    }
 
     @PostMapping(value = "login")
     public ResponseEntity<UserDto> login(@RequestBody LoginDto login){
         User user = userService.getUser(login.getName(), login.getPassword());
         String token = generateTokenFor(user);
         UserDto userDto = UserMapper.convertToDto(user);
+
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " + token);
         return ResponseEntity.status(HttpStatus.OK).headers(headers).body(userDto);
     }
     @PostMapping(value = "register")
     public ResponseEntity<UserDto> register(@RequestBody RegisterDto register){
-        AuthValidator.getInstance().ValidateRegister(register);
+        AuthValidator.validateRegister(register);
+
         User newUser = userService.createUser(register.getName(), register.getPassword());
         String token = generateTokenFor(newUser);
         UserDto userDto = UserMapper.convertToDto(newUser);
+
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", "Bearer " + token);
         return ResponseEntity.status(HttpStatus.OK).headers(headers).body(userDto);

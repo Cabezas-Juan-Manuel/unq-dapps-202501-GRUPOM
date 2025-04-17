d14d684d22e04de9b0fe364e9ba61c45d5bf577e
Matias Laime <matulai1221@gmail.com>
2025-04-17 20:33:59 -0300

    Refactor: change to constructor inyection and rename var and methods

--- src/main/java/ar/edu/unq/pronosticodeportivo/utils/AuthValidator.java
+++ src/main/java/ar/edu/unq/pronosticodeportivo/utils/AuthValidator.java
@@ -6,23 +6,16 @@ import ar.edu.unq.pronosticodeportivo.webservice.Dtos.RegisterDto;
 
 public class AuthValidator {
 
-    private static AuthValidator instance;
-    private static final String passwordRegex= "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$";
+    private static final String PASSWORDREGEX = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!@#$%^&*])[A-Za-z\\d!@#$%^&*]{8,}$";
 
+    private AuthValidator() { }
 
-    public static AuthValidator getInstance() {
-        if (instance == null){
-            instance = new AuthValidator();
-        }
-        return instance;
-    }
-
-    public void ValidateRegister(RegisterDto register) {
+    public static void validateRegister(RegisterDto register) {
         itsAnInValidPassword(register.getPassword());
     }
 
-    private void itsAnInValidPassword(String password) {
-        if(!Pattern.matches(passwordRegex, password)) {
+    private static void itsAnInValidPassword(String password) {
+        if(!Pattern.matches(PASSWORDREGEX, password)) {
             throw new IllegalArgumentException("La contrasenia no es correcta");
         }
     }
